17d348cdccd060d9b36b2c37098cfdff9696ec38
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-03 15:38:31 -0300

    requsito-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        {/* <Switch> */}
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" render={ () => <Album /> } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route component={ NotFound } />
+        {/* </Switch> */}
+      </BrowserRouter>
+    );
   }
 }
 
