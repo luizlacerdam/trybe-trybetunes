a9369f7787913d2f75b39376eb792a37cacc560e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-10-05 15:45:31 -0300

    requsito-5-Header-em-components

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Header from '../components/Header';
 
 class Album extends React.Component {
   render() {
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import Header from './Header';
+import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleInputOnChange = (event) => {
+    const { value } = event.target;
+    this.setState({ inputValue: value });
+  };
+
   render() {
+    const { inputValue } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleInputOnChange }
+            value={ inputValue }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ (inputValue.length < 2) }
+          >
+            Pesquisar
+          </button>
+        </div>
       </>
     );
   }
